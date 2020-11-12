5e821782133ea7ff479248dd9cd3657c779994c9
HarikaYarlagadda <73021630+HarikaYarlagadda@users.noreply.github.com>
2020-11-12 12:53:41 +0530

    Added code for getting username and password details using jdbc

---
+++ Demojdbc.java
@@ -0,0 +1,25 @@
+package jdbcDemo;
+
+import java.sql.*;
+public class Demojdbc {
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		String url="jdbc:mysql://localhost:3306/logindata";
+		String uname="root";
+		String pass="RootPassword";
+		String query = "select * from userdetails1";
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection con = DriverManager.getConnection(url,uname,pass);
+		Statement st =con.createStatement();
+		ResultSet rs=st.executeQuery(query);
+		while(rs.next()) {
+			
+		String name = rs.getString("uname");
+		String password=rs.getString("pass");
+		System.out.println(name+" "+password);
+		}
+		st.close();
+		con.close();
+	}
+
+}
