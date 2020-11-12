bb8c4821df77e4c1e72122bee3bc19ab231a08cd
HarikaYarlagadda <73021630+HarikaYarlagadda@users.noreply.github.com>
2020-11-12 13:01:51 +0530

    Add files via upload

---
+++ loginData.sql
@@ -0,0 +1,9 @@
+use logindata;
+SELECT * FROM userdetails1;
+insert into userdetails1 values('mr.bean','teddy');
+insert into userdetails1 values('tom','jerry');
+insert into userdetails1 values('phineas','ferb');
+insert into userdetails1 values('bheem','chutki');
+insert into userdetails1 values('harika','1234');
+insert into userdetails1 values('harish','jam1234');
+insert into userdetails1 values('popeye','spinach03');
